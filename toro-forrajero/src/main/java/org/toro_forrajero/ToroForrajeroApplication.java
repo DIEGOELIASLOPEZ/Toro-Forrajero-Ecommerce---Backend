e2f5d0571274c6b721b770bcbbfc0f336001f12d
Elias López Martínez <91902204+DIEGOELIASLOPEZ@users.noreply.github.com>
2026-08-20 17:09:15 -0600

    Tarea 12 - Se crea repositorio y proyecto de SpringBoot

---
+++ toro-forrajero/src/main/java/org/toro_forrajero/ToroForrajeroApplication.java
@@ -0,0 +1,13 @@
+package org.toro_forrajero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToroForrajeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToroForrajeroApplication.class, args);
+	}
+
+}
---
+++ toro-forrajero/src/test/java/org/toro_forrajero/ToroForrajeroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.toro_forrajero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToroForrajeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

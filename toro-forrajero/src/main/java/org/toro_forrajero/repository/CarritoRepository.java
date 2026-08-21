3b5ed4f3bb3b31e243fbfb1cb1e9470706a992e0
David Vargas <boutvargas@gmail.com>
2026-08-21 16:38:43 -0600

    feat: agregada entidad, ID compuesto y repositorio para Carrito

---
+++ toro-forrajero/src/main/java/org/toro_forrajero/repository/CarritoRepository.java
@@ -0,0 +1,12 @@
+package org.toro_forrajero.repository;
+
+import org.toro_forrajero.model.Carrito;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CarritoRepository extends JpaRepository<Carrito, Long> {
+    // Método útil para buscar el carrito activo de un usuario específico
+    Optional<Carrito> findByUsuarioIdUsuario(Long idUsuario);
+}

1a44a6562d41abb02cbe3477df93bc33584d650c
Daniela Tobón <daniela.tobon@anahuac.mx>
2026-08-21 16:34:16 -0600

    TF12: Creación de detalle carrito, modelo y repositorio

---
+++ toro-forrajero/src/main/java/org/toro_forrajero/repository/DetalleCarritoRepository.java
@@ -0,0 +1,15 @@
+package org.toro_forrajero.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.toro_forrajero.model.DetalleCarrito;
+import org.toro_forrajero.model.DetalleCarritoId;
+import java.util.List;
+
+public interface DetalleCarritoRepository extends JpaRepository<DetalleCarrito, DetalleCarritoId> {
+
+    // Buscar todos los items que pertenecen a un carrito específico
+    List<DetalleCarrito> findByCarritoIdCarrito(Long idCarrito);
+
+    // Eliminar los productos de un carrito al vaciarlo
+    void deleteByCarritoIdCarrito(Long idCarrito);
+}

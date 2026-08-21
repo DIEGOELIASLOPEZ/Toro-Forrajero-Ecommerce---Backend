e68cbdaeb4a081ef6c704ff53cc667930ac2029b
DianaH-314 <dianalau230998@gmail.com>
2026-08-21 13:36:13 -0600

    TF12 Feature: creación de ProductosRepository

---
+++ toro-forrajero/src/main/java/org/toro_forrajero/repository/ProductosRepository.java
@@ -0,0 +1,23 @@
+package org.toro_forrajero.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.toro_forrajero.model.Productos;
+
+@Repository
+public interface ProductosRepository extends JpaRepository<Productos, Long>{
+    //Mostrar productos por marca
+    List<Productos> findByMarcaIgnoreCase(String marca);
+
+    //Mostrar productos por especie
+    List<Productos> findByEspecieIgnoreCase(String especie);
+
+    //Mostrar productos por marca && especie - a ver si funciona
+    List<Productos> findByMarcaIgnoreCaseAndespecieIgnoreCase(
+            String marca,
+            String especie
+    );
+}

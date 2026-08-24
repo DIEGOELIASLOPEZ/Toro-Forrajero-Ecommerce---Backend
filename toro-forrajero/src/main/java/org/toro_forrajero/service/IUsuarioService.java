e154b10031018001feb462ebc03436bfda1a4812
natalia-susana <nataliasusanacruzruiz@gmail.com>
2026-08-24 17:09:42 -0600

    Se agrega IUsuarioService y UsuarioService

---
+++ toro-forrajero/src/main/java/org/toro_forrajero/service/IUsuarioService.java
@@ -0,0 +1,20 @@
+package org.toro_forrajero.service;
+
+import java.util.List;
+
+public interface IUsuarioService {
+
+    List<UsuarioDTO.UsuarioResponse> mostrarUsuarios();
+
+    UsuarioDTO.UsuarioResponse mostrarPorId(Long id);
+
+    UsuarioDTO.UsuarioResponse crearUsuario(UsuarioDTO.UsuarioRequest usuarioRequest);
+
+    UsuarioDTO.UsuarioResponse actualizarUsuario(Long id, UsuarioDTO.UsuarioRequest usuarioRequest);
+
+    void eliminarUsuario(Long id);
+
+    UsuarioDTO.UsuarioResponse mostrarPorCorreo(String correo);
+
+    boolean existePorCorreo(String correo);
+}

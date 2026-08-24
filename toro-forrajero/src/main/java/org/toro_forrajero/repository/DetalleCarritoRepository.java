0c3131434477878d6e66670a4fe4cc8069ef0296
Daniela Tobón <daniela.tobon@anahuac.mx>
2026-08-24 11:37:44 -0600

    TF12 - Fix: Carrito y detalle carrito Se corrigió las clases de carrito y detalle carrito según las especificaciones de las otras clases

--- toro-forrajero/src/main/java/org/toro_forrajero/repository/DetalleCarritoRepository.java
+++ toro-forrajero/src/main/java/org/toro_forrajero/repository/DetalleCarritoRepository.java
@@ -1,15 +1,17 @@
-//package org.toro_forrajero.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.toro_forrajero.model.DetalleCarrito;
-//import org.toro_forrajero.model.DetalleCarritoId;
-//import java.util.List;
-//
-//public interface DetalleCarritoRepository extends JpaRepository<DetalleCarrito, DetalleCarritoId> {
-//
-//    // Buscar todos los items que pertenecen a un carrito específico
-//    List<DetalleCarrito> findByCarritoIdCarrito(Long idCarrito);
-//
-//    // Eliminar los productos de un carrito al vaciarlo
-//    void deleteByCarritoIdCarrito(Long idCarrito);
-//}
+package org.toro_forrajero.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+import org.toro_forrajero.model.DetalleCarrito;
+import org.toro_forrajero.model.DetalleCarritoId;
+import java.util.List;
+
+public interface DetalleCarritoRepository extends JpaRepository<DetalleCarrito, DetalleCarritoId> {
+
+    List<DetalleCarrito> findByCarrito_IdCarrito(Long idCarrito);
+
+    @Transactional
+    @Modifying
+    void deleteByCarrito_IdCarrito(Long idCarrito);
+}

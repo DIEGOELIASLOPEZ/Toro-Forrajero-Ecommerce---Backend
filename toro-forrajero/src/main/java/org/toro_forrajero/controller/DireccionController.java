81bd2fdf1501fb99c8e6de66c0fcae7dd73ce837
VanessaEstrada04 <vanessa.estrada.arellano99@.com>
2026-08-25 17:29:38 -0600

    Fix en la etiqueta para peticiones en direccion

--- toro-forrajero/src/main/java/org/toro_forrajero/controller/DireccionController.java
+++ toro-forrajero/src/main/java/org/toro_forrajero/controller/DireccionController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/direcciones")
+@CrossOrigin(origins = "*") // Permite peticiones desde el frontend
 
 
 public class DireccionController {
